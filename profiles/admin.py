e191ae788bff1048765ce67c1b33b01d636b48e3
Mark Jun A. Gersaniva <markjungersaniva@gmail.com>
2019-08-16 22:36:44 +0800

    Reconstructed profiles module

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,26 +1,18 @@
 from django.contrib import admin
 
-from profiles.models import BaseProfile, ProfileMobtel, Gender
+# Register your models here.
+from profiles.models import Profile
+from profiles.models.profile_models import Gender
 
 
-class BaseProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'first_name', 'last_name', 'rank')
-    list_filter = ('gender', 'rank')
-    search_fields = ('first_name', 'last_name', 'user', 'rank')
-
-admin.site.register(BaseProfile, BaseProfileAdmin)
-
-
-class ProfileMobtelAdmin(admin.ModelAdmin):
-    list_display = ('number', 'carrier', 'is_public', 'is_active')
-    list_filter = ('number', 'carrier', 'is_public', 'is_active')
-    search_fields = ('number', 'carrier')
-
-admin.site.register(ProfileMobtel, ProfileMobtelAdmin)
+class ProfileAdmin(admin.ModelAdmin):
+  list_display = ('account', 'first_name', 'last_name',)
+  search_fields = ('first_name', 'middle_name', 'last_name',)
 
 
 class GenderAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    list_filter = ('name',)
+  list_display = ('name',)
+  search_fields = ('name',)
 
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Gender, GenderAdmin)
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,20 +1,10 @@
 from django.urls import path
 
-from profiles.modules.views import home as home_views
-from profiles.modules.views import address as address_views
-from profiles.modules.views import mobtels as mobtel_views
+from profiles.controllers.views import profile_views
 
 urlpatterns = [
-    path('home', home_views.ProfileHomeView.as_view(), name='profiles_home_view'),
-    path('create', home_views.ProfileCreateView.as_view(), name='profiles_create_view'),
-    path('update', home_views.ProfileUpdateView.as_view(), name='profiles_update_view'),
-
-    path('address/create', address_views.ProfileAddressCreateView.as_view(), name='profiles_address_create_view'),
-    path('address/<pk>/update', address_views.ProfileAddressUpdateView.as_view, name='profiles_address_update_view'),
-    path('address/<pk>/delete', address_views.ProfileAddressDeleteView.as_view(), name='profiles_address_delete_view'),
-
-    path('mobtel/create', mobtel_views.ProfileMobtelCreateView.as_view(), name='profiles_mobtel_create_view'),
-    path('mobtel/<pk>/update', mobtel_views.ProfileMobtelUpdateView.as_view(), name='profile_mobtel_update_view'),
-    path('mobtel/<pk>/delete', mobtel_views.ProfileMobtelDeleteView.as_view(), name='profile_mobtel_delete_view'),
-
+    path('', profile_views.ProfileHomeView.as_view(), name='profile_home_view'),
+    path('update/', profile_views.ProfileUpdateView.as_view(), name='profile_update_view'),
+    path('account/update/', profile_views.AccountUpdateView.as_view(), name='account_update_view'),
+    path('account/password/', profile_views.AccountPasswordUpdateView.as_view(), name='account_password_view'),
 ]
