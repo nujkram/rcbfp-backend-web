cf24df60355fb29bad8f78769e3ac9280a0343c4
Mark <markjungersaniva@gmail.com>
2020-11-20 22:37:10 +0800

    Reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, \
+    PasswordResetCompleteView
 
 from accounts.controllers.views.account_views import AccountLoginView, AccountLogoutView
 
 urlpatterns = [
     path('login', AccountLoginView.as_view(), name='accounts_login'),
     path('logout', AccountLogoutView.as_view(), name='accounts_logout'),
+    path('password/reset/', PasswordResetView.as_view(),
+         {'post_reset_redirect': '/account/password/reset/done/'},
+         name="password_reset"),
+    path('password/reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password/reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
+         {'post_reset_redirect': '/account/password/complete/'},
+         name='password_reset_confirm'
+         ),
+    path('password/reset/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
