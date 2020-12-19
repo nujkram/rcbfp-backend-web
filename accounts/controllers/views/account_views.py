a46b111fa4ba1e6012812163b05bdbff8401fae2
Mark <markjungersaniva@gmail.com>
2020-12-19 11:15:07 +0800

    Password reset

--- accounts/controllers/views/account_views.py
+++ accounts/controllers/views/account_views.py
@@ -1,67 +1,104 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.tokens import default_token_generator
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.contrib.auth.views import PasswordContextMixin
 
 from accounts.constants import USER_DASHBOARD_ROOTS
-from accounts.controllers.views.forms.account_forms import LoginForm
+from accounts.controllers.views.forms.account_forms import LoginForm, AccountPasswordResetForm
+from django.urls import reverse, reverse_lazy
+from django.views.generic import FormView
+from django.views.decorators.csrf import csrf_protect
 
 
 class AccountLoginView(View):
-  def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
 
-    if request.user.is_authenticated:
-      return HttpResponseRedirect(f'/{USER_DASHBOARD_ROOTS[request.user.user_type]}')
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(f'/{USER_DASHBOARD_ROOTS[request.user.user_type]}')
 
-    form = LoginForm
+        form = LoginForm
 
-    context = {
-      "page_title": f"RCBFP: Login",
-      "form": form,
-      "location": "login"
-    }
+        context = {
+            "page_title": f"RCBFP: Login",
+            "form": form,
+            "location": "login"
+        }
 
-    return render(request, "accounts/login.html", context)
+        return render(request, "accounts/login.html", context)
 
-  def post(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
 
-    form = LoginForm(data=request.POST)
+        form = LoginForm(data=request.POST)
 
-    if form.is_valid():
-      username = form.cleaned_data.get('username')
-      password = form.cleaned_data.get('password')
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
 
-      user = authenticate(username=username, password=password)
+            user = authenticate(username=username, password=password)
 
-      if user is not None:
-        if not user.is_active:
-          messages.error(
-            request, "User account is not active", extra_tags="danger"
-          )
+            if user is not None:
+                if not user.is_active:
+                    messages.error(
+                        request, "User account is not active", extra_tags="danger"
+                    )
+                else:
+                    login(request, user)
+                    messages.success(request, f"Welcome, {user}!", extra_tags="success")
+
+                    next = request.GET.get('next', None)
+                    if next:
+                        return HttpResponseRedirect(next)
+                    return HttpResponseRedirect(f'/{USER_DASHBOARD_ROOTS[user.user_type]}')
+            else:
+                messages.error(request, "Invalid credentials", extra_tags="danger")
         else:
-          login(request, user)
-          messages.success(request, f"Welcome, {user}!", extra_tags="success")
+            messages.error(request, form.errors, extra_tags="danger")
 
-          next = request.GET.get('next', None)
-          if next:
-            return HttpResponseRedirect(next)
-          return HttpResponseRedirect(f'/{USER_DASHBOARD_ROOTS[user.user_type]}')
-      else:
-        messages.error(request, "Invalid credentials", extra_tags="danger")
-    else:
-      messages.error(request, form.errors, extra_tags="danger")
-
-    context = {
-      "page_title": f"Login",
-      "form": form,
-      "location": "login"
-    }
-    return render(request, "accounts/login.html", context)
+        context = {
+            "page_title": f"Login",
+            "form": form,
+            "location": "login"
+        }
+        return render(request, "accounts/login.html", context)
 
 
 class AccountLogoutView(View):
-  def get(self, request):
-    logout(request)
-    return HttpResponseRedirect(reverse("accounts_login"))
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse("accounts_login"))
+
+
+class PasswordResetView(PasswordContextMixin, FormView):
+    email_template_name = 'accounts/password_reset_email.html'
+    extra_email_context = None
+    form_class = AccountPasswordResetForm
+    from_email = None
+    html_email_template_name = None
+    subject_template_name = 'password_reset/password_reset_subject.txt'
+    success_url = reverse_lazy('password_reset_done')
+    template_name = 'accounts/password_reset_form.html'
+    title = 'Password reset'
+    token_generator = default_token_generator
+
+    @method_decorator(csrf_protect)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        opts = {
+            'use_https': self.request.is_secure(),
+            'token_generator': self.token_generator,
+            'from_email': self.from_email,
+            'email_template_name': self.email_template_name,
+            'subject_template_name': self.subject_template_name,
+            'request': self.request,
+            'html_email_template_name': self.html_email_template_name,
+            'extra_email_context': self.extra_email_context,
+        }
+        form.save(**opts)
+        return super().form_valid(form)
--- accounts/controllers/views/forms/account_forms.py
+++ accounts/controllers/views/forms/account_forms.py
@@ -1,6 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm, SetPasswordForm, \
-    ReadOnlyPasswordHashField
+    ReadOnlyPasswordHashField, PasswordResetForm
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMultiAlternatives
+from django.template import loader
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.utils.translation import gettext_lazy as _
 
 from accounts.models import Account
 
@@ -55,3 +62,69 @@ class UpdatePasswordForm(forms.Form):
         if password != confirm_password:
             raise forms.ValidationError("Your passwords don't match!")
 
+
+class AccountPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(label=_("Email"), max_length=254)
+
+    def send_mail(self, subject_template_name, email_template_name,
+                  context, from_email, to_email, html_email_template_name=None):
+        """
+        Send a django.core.mail.EmailMultiAlternatives to `to_email`.
+        """
+        subject = loader.render_to_string(subject_template_name, context)
+        # Email subject *must not* contain newlines
+        subject = ''.join(subject.splitlines())
+        body = loader.render_to_string(email_template_name, context)
+
+        email_message = EmailMultiAlternatives(subject, body, from_email, [to_email])
+        if html_email_template_name is not None:
+            html_email = loader.render_to_string(html_email_template_name, context)
+            email_message.attach_alternative(html_email, 'text/html')
+
+        email_message.send()
+
+    def get_users(self, email):
+        """Given an email, return matching user(s) who should receive a reset.
+
+        This allows subclasses to more easily customize the default policies
+        that prevent inactive users and users with unusable passwords from
+        resetting their password.
+        """
+        active_users = Account._default_manager.filter(**{
+            '%s__iexact' % Account.get_email_field_name(): email,
+            'active': True,
+        })
+        return (u for u in active_users if u.has_usable_password())
+
+    def save(self, domain_override=None,
+             subject_template_name='accounts/password_reset_subject.txt',
+             email_template_name='accounts/password_reset_email.html',
+             use_https=False, token_generator=default_token_generator,
+             from_email=None, request=None, html_email_template_name=None,
+             extra_email_context=None):
+        """
+        Generate a one-use only link for resetting password and send it to the
+        user.
+        """
+        email = self.cleaned_data["email"]
+        for user in self.get_users(email):
+            if not domain_override:
+                current_site = get_current_site(request)
+                site_name = current_site.name
+                domain = current_site.domain
+            else:
+                site_name = domain = domain_override
+            context = {
+                'email': email,
+                'domain': domain,
+                'site_name': site_name,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'user': user,
+                'token': token_generator.make_token(user),
+                'protocol': 'https' if use_https else 'http',
+                **(extra_email_context or {}),
+            }
+            self.send_mail(
+                subject_template_name, email_template_name, context, from_email,
+                email, html_email_template_name=html_email_template_name,
+            )
